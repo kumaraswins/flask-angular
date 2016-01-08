03423fdfd4d6eed5235716a8470d8728b4202060
Aswinkumar Santhanam <aswinkumar@tagitmobile.com>
2016-01-08 17:39:57 +0530

    sample rest calll

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,16 @@ from flask import render_template, url_for, redirect, send_from_directory
 from flask import send_file, make_response, abort
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello_world():    
     return send_file("templates/index.html")
+
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():    
+    print ("THis is a test")
+    return 'The project page'
 
 if __name__ == '__main__':
     app.run()
--- app/static/js/controller/DashboardController.js
+++ app/static/js/controller/DashboardController.js
@@ -17,6 +17,17 @@ function DashboardController($scope, $http, $location, DataService) {
         // called asynchronously if an error occurs
         // or server returns response with an error status.
     });
+    
+    $http({
+        method: 'GET',
+        url: '/projects'
+    }).then(function successCallback(response) {
+        console.log(response.data);
+
+    }, function errorCallback(response) {
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
 
     $scope.submit = function () {
         DataService.addProduct($scope.user);
